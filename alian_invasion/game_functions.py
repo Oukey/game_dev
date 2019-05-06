dbf93a1aff03c395944b622ec90be08ff20e0a57
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-06 22:16:53 +0400

    Update game_functions.py

--- alian_invasion/game_functions.py
+++ alian_invasion/game_functions.py
@@ -4,12 +4,31 @@ import sys
 
 import pygame
 
+def check_keydown_events(event, ship):
+    '''Реагирует на нажатие клавиш.'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+        
+        
+def check_keyup_events(event, ship):
+    '''Реагирует на отпускание клавишь'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
-def chec_events():
+        
+def chec_events(ship):
     '''Обрабатывает нажатия клавишь и события мыши.'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settimgs, screen, ship):
