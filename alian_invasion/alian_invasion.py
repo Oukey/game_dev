553e01ff8ea812ef4b51bf932cab51ce88de479d
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-06 22:15:13 +0400

    Update alian_invasion.py

--- alian_invasion/alian_invasion.py
+++ alian_invasion/alian_invasion.py
@@ -9,8 +9,6 @@ import game_functions as gf
 
 def run_game():
     pygame.display.set_caption("Alian_Invasion")
-    # Назначение цвета фона.
-    # bg_color = (230, 230, 230)
 
     # Инициализирует игру и создает объект экрана.
     pygame.init()
@@ -18,19 +16,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 
     # Создание корабля
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # запуск основного цикла
     while True:
         # Отслеживание событий клавиатуры мыши.
-        gf.chec_events()  # Цикл событий из модуля game_functions.py
-
-
-        # При каждом проходе цикла перерисовывается экран.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()  # прорисовка корабля при исполнении основного цикла
-        # Отображение последнего прорисованного экрана.
-        pygame.display.flip()
+        gf.chec_events(ship)  # Цикл событий из модуля game_functions.py
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
