b1344a6c08833273d3f5c74f687ebb8576f30fb0
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-22 02:00:32 +0400

    Update settings.py

--- alian_invasion/settings.py
+++ alian_invasion/settings.py
@@ -11,8 +11,15 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (50, 140, 180)
         
+        # Настройки пришельца
+        self.alien_speed_factor = 1
+        
         # настройки корабля
         self.ship_speed_factor = 1.5
+        
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1  # обозначает движение вправо; a - 1 - влево
+        self.fleet_direction = 1
         
         # Параметры пули
         self.bullet_speed_factor = 1
