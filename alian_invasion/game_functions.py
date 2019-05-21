47e41b9e7f5501c493bf90032f2e580f10f560af
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-22 01:59:25 +0400

    Update game_functions.py

--- alian_invasion/game_functions.py
+++ alian_invasion/game_functions.py
@@ -105,4 +105,27 @@ def create_fleet(ai_settings, screen, ship, aliens):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
+
+def check_flet_edges(ai_settings, aliens):
+    '''Реагирует на достижение пришельцем края экрана'''
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    '''Опускает весь флот и меняет направление флота'''
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
+    '''
+    Обновляет позиции всех пришельцев во флоте
+    после чего обновяет позиции всех пришельцев во флоте
+    '''
+    check_flet_edges(ai_settings, aliens)
+    aliens.update()
         
