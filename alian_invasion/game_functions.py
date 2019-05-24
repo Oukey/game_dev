b2bfb56fd01979994a5cad7c524c4768e2e32203
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-24 10:46:31 +0400

    update

--- alian_invasion/game_functions.py
+++ alian_invasion/game_functions.py
@@ -15,10 +15,12 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.ey == pygame.K_q:
+        sys.exit()
 
 
 def check_keyup_events(event, ship):
-    '''Реагирует на отпускание клавишь'''
+    '''Реагирует на отпускание клавиш'''
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
@@ -26,7 +28,7 @@ def check_keyup_events(event, ship):
 
 
 def check_events(ai_settings, screen, ship, bullets):
-    '''Обрабатывает нажатия клавишь и события мыши'''
+    '''Обрабатывает нажатия клавиш и события мыши'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -37,7 +39,7 @@ def check_events(ai_settings, screen, ship, bullets):
 
 
 def update_screen(ai_settings, screen, ship, aliens, bullets):
-    '''Обновяет изображения на экране и отображает новый экран.'''
+    '''Обновляет изображения на экране и отображает новый экран.'''
     # При каждом проходе цикла перерисовывает экран.
     screen.fill(ai_settings.bg_color)
     # Все пули выводятся позади изображений корабля и пришельцев
@@ -50,15 +52,27 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     '''Обновляет позиции пуль и уничтожает старые пути'''
     # Обновление позиций пуль
     bullets.update()
 
-    # Удаление пуль, вышедших за край экрана
+    # Уничтожение исчезнуыших пуль
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    '''Обработка коллизий пуль с пришельцами'''
+    # Удаление пуль и пришельцев, учатсвующих в коллизиях
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # Уничтожение существующих пуль и создание нового флота
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
@@ -67,7 +81,8 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
-        
+
+
 def get_number_aliens_x(ai_settings, alien_width):
     '''Вычисляет количество пришельцев в ряду'''
     available_space_x = ai_settings.screen_width - 2 * alien_width
@@ -90,11 +105,11 @@ def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     alien.rect.x = alien.x
     alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
-   
+
 
 def create_fleet(ai_settings, screen, ship, aliens):
     '''Создает флот пришельцев'''
-    # Создание пришельца и вычисление количества пришеьцев в ряду
+    # Создание пришельца и вычисление количества пришельцев в ряду
     alien = Alien(ai_settings, screen)
     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
     number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
@@ -124,8 +139,7 @@ def change_fleet_direction(ai_settings, aliens):
 def update_aliens(ai_settings, aliens):
     '''
     Обновляет позиции всех пришельцев во флоте
-    после чего обновяет позиции всех пришельцев во флоте
+    после чего обновляет позиции всех пришельцев во флоте
     '''
     check_flet_edges(ai_settings, aliens)
     aliens.update()
-        
