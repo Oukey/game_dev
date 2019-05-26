6031c7d63b4af748750fac3df6c97d9c7835383e
Eugene <42565803+Oukey@users.noreply.github.com>
2019-05-26 22:25:28 +0400

    Update button.py

--- alian_invasion/button.py
+++ alian_invasion/button.py
@@ -16,7 +16,7 @@ class Button():
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
-        # Постоение объекта rect кнопки и выравнивание по центру экрана
+        # Построение объекта rect кнопки и выравнивание по центру экрана
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
 
